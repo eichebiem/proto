c668fba38586a78fa6a4a0e3d143dc06042640fd
Harris Mariano <32866354+eichebiem@users.noreply.github.com>
2018-04-17 14:13:36 +0800

    Modified Login interface

---
+++ public/js/vendor/form-validation.js
@@ -0,0 +1,96 @@
+$(document).ready(function(){
+
+     $('#alert_message').delay(10000).fadeOut();
+
+     $.validator.setDefaults({
+          errorClass: 'help-block',
+          highlight: function(element) {
+              $(element)
+                  .closest('.form-group')
+                  .addClass('has-error');
+          },
+          unhighlight: function(element) {
+              $(element)
+                  .closest('.form-group')
+                  .removeClass('has-error');
+          },
+          errorPlacement: function (error, element) {
+              if (element.prop('type') === 'checkbox') {
+                  error.insertAfter(element.parent());
+              } else {
+                  error.insertAfter(element);
+              }
+          }
+      });
+
+     $('#form-validate').validate({
+
+          rules: {
+               username: {
+                    required: true,
+                    minlength: 5
+               },
+               password: {
+                    required: true,
+                    minlength: 5
+               }
+          },
+
+          messages: {
+               
+          },
+
+          submitHandler: function(form){
+               form.submit();
+          }
+
+     });
+
+
+     // Input mask
+//     $('#contact_number').mask('0000-000-0000');
+//     $('#company_tel').mask('(000) 000-0000');
+//     $('#client_tel').mask('(000) 000-0000');
+
+
+    // make check boxes behave like radio button
+//     $(".course").change(function() {
+//         $(".course").prop('checked', false);
+//         $(this).prop('checked', true);
+//     });
+
+
+//     $(".ewan").change(function() {
+//         $(".ewan").prop('checked', false);
+//         $(this).prop('checked', true);
+//     });
+
+
+//     $(".days").change(function() {
+//         $(".days").prop('checked', false);
+//         $(this).prop('checked', true);
+//     });
+
+
+//     $('.ewan').change(function () {
+//         //check if checkbox is checked
+//         if ($('#inter').is(':checked')) {
+
+//             $('.days').removeAttr('disabled'); //enable input
+
+//         } else {
+//             $('.days').removeAttr('checked');
+//             $('.days').attr('disabled', true); //disable input
+//         }
+//     });
+
+
+//     $(".research_class").change(function() {
+//         $(".research_class").prop('checked', false);
+//         $(this).prop('checked', true);
+//     });
+
+
+});
+
+
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Log in</title>
+  <title>Elementary | Admin</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -31,20 +31,20 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
+    <a href="../../index2.html"><b>Elementary</b> Admin</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
 
-    <form action="../../index2.html" method="post">
+    <form action="" method="POST" id="form-validate">
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        <input type="text" class="form-control" placeholder="Username" name="username">
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        <input type="password" class="form-control" placeholder="Password" name="password">
       </div>
       <div class="row">
         <div class="col-xs-8">
@@ -76,6 +76,11 @@
 <script src="/js/vendor/bootstrap.min.js"></script>
 <!-- iCheck -->
 <script src="/js/plugins/icheck.min.js"></script>
+
+<!-- jQuery Validate -->
+<script src="/js/vendor/jquery.validate.min.js"></script>
+<!-- Custom Form Validation -->
+<script src="/js/vendor/form-validation.js"></script>
 <script>
   $(function () {
     $('input').iCheck({
